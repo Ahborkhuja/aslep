b2281cca0f9a0977047def89098bf8117642b775
ahborkhuja <93854287+Ahborkhuja@users.noreply.github.com>
2022-11-03 16:56:50 +0500

    upd

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const bur_open = document.querySelector('.nav__inner_list')
 const btn = document.querySelector('.site_header__burger');
-const btns_cl = document.querySelector('.nav__btns');
+const site_navbar = document.querySelector('.site_header__nav');
+const phn_num= document.querySelector('.nav__select');
+const btns_cl = document.querySelector('.nav__inner_btns');
 const body = document.querySelector('body');
 const site_head = document.querySelector('.site_header');
 // const h2 = document.querySelector('.hero_inner h2');
 btn.addEventListener('click', () => {
   bur_open.classList.toggle('open');
+  site_navbar.classList.toggle('open');
+  phn_num.classList.toggle('open');
   btns_cl.classList.toggle('open');
   body.classList.toggle('overflow');
   site_head.classList.toggle('bgcol');
@@ -15,6 +19,9 @@ btn.addEventListener('click', () => {
 window.addEventListener("resize", function () {
   if (window.matchMedia("(max-width: 550px)").matches) {
     bur_open.classList.remove('open');
+    site_navbar.classList.remove('open');
+    phn_num.classList.remove('open');
+    btns_cl.classList.remove('open');
     site_head.classList.remove('bgcol');
     btn.classList.remove('exit');
     body.classList.remove('overflow');
